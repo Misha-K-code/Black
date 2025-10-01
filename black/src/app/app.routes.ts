692793441a87e13e39353ff85b30abee2f2a3a5b
Statham <kistrujmisa@gmail.com>
2025-10-01 10:56:53 +0300

    Fix project

--- black/src/app/app.routes.ts
+++ black/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FilmDetails } from './film-details/film-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'film', component: FilmDetails },
+];
--- black/src/main.ts
+++ black/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { FilmDetails } from './app/film-details/film-details';
+import { App } from './app/app';
 
-bootstrapApplication(FilmDetails, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
