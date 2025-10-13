38e32015d6ae4dda3440f1ff4f71cdacbe0cf921
Skala69 <artyrz@pss1.ukr.education>
2025-10-13 09:09:24 +0300

    Add film search

--- black/src/app/app.routes.ts
+++ black/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FilmDetails } from './film-details/film-details';
+import { FilmSearch } from './film-search/film-search';
 
 export const routes: Routes = [
   { path: 'film', component: FilmDetails },
+  { path: 'search', component: FilmSearch },
 ];
---
+++ black/src/app/film-search/film-search.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-search',
+  imports: [],
+  templateUrl: './film-search.html',
+  styleUrl: './film-search.css'
+})
+export class FilmSearch {
+
+}
