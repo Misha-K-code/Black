4ebc139bb5fd2ee4415e1604b08f7f5fa2db6ea3
koshik1122 <butkogrisa252@gmail.com>
2025-10-20 09:11:40 +0300

    |Add raiting component

--- black/src/app/app.routes.ts
+++ black/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FilmDetails } from './film-details/film-details';
 import { FilmSearch } from './film-search/film-search';
+import { FilmRaiting } from './film-raiting/film-raiting';
 
 export const routes: Routes = [
   { path: 'film', component: FilmDetails },
   { path: 'search', component: FilmSearch },
+  { path: 'raiting', component: FilmRaiting },
 ];
---
+++ black/src/app/film-raiting/film-raiting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-raiting',
+  imports: [],
+  templateUrl: './film-raiting.html',
+  styleUrl: './film-raiting.css'
+})
+export class FilmRaiting {
+
+}
