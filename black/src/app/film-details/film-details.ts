f2f20ffebb80df9f930d90d9cbd2e6b81b6b2f93
Statham <kistrujmisa@gmail.com>
2025-12-17 13:57:09 +0200

    work

--- black/src/app/film-details/film-details.ts
+++ black/src/app/film-details/film-details.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-film-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-details.html',
   styleUrl: './film-details.css'
 })
